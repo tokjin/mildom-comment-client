0bc7f778d016ad20495eee01c4fa923032f8f915
tokjin <tkd3@i.softbank.jp>
2020-01-26 01:37:26 +0900

    v1.2.2

--- js/update-check.js
+++ js/update-check.js
@@ -3,7 +3,7 @@ let getGitRepos = () => {
         let latestVer = res[0].tag_name;
         if(latestVer != currentVer) {
             noticeDraw('コメントビューアのアップデートが見つかりました。('+currentVer+'->'+latestVer+')', 'info');
-            noticeDraw('最新バージョンは<a href="https://github.com/tokjin/mildom-comment-client/releases/tag/v1.1.1">こちら</a>からダウンロードできます。', 'info');
+            noticeDraw('最新バージョンは<a href="https://github.com/tokjin/mildom-comment-client/releases/latest" target="_blank">こちら</a>からダウンロードできます。', 'info');
         }
     }).fail(function (res) { console.log('getGitRepos failed'); });
 }
